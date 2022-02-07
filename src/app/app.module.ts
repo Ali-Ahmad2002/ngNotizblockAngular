3a705a443e8eb16853f097e5ce06bf04746b1f1b
Ali-Ahmad2002 <aliahmad.557755@gmail.com>
2022-02-07 04:43:44 +0100

    add-noteSection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { NotesAreaComponent } from './notes-area/notes-area.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NotesAreaComponent
   ],
   imports: [
     BrowserModule,
